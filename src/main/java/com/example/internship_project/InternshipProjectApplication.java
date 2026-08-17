1204d97104a8cc71a34387fe7c2c6ee8aa19fdda
vert2kys <westreserv@gmail.com>
2026-08-17 17:28:30 +0200

    Add Spring Boot project structure

---
+++ src/main/java/com/example/internship_project/InternshipProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.internship_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipProjectApplication.class, args);
+	}
+
+}
